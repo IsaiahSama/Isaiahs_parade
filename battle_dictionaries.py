038e9c703ea6e7579678d92b340fe6dde7eff525
Isaiah-Sama <zeldaplayergl@gmail.com>
2021-04-13 18:34:43 -0400

    Moved dictionaries to another file. Finished skeleton for blessings

---
+++ battle_dictionaries.py
@@ -0,0 +1,145 @@
+# Player Template
+player = {
+    "NAME": "",
+    "LEVEL": 0,
+    "TIER": 1,
+    "CLASS": "",
+    "HEALTH": 100,
+    "POWER": 30,
+    "DEFENSE": 10,
+    "CRIT_CHANCE": 5,
+    "ABILITY_1": "None",
+    "ABILITY_2": "None",
+    "PARADIANS": 100,
+    "WEAPON": "None",
+    "ARMOR": "None",
+    "EXP": 0,
+    "EXP_TO_NEXT_LEVEL": 100,
+    "PLAYER_ID": 0
+}
+
+# Enemy Template
+enemy = {
+    "NAME": "",
+    "HEALTH": 100,
+    "POWER": 0,
+    "DEFENSE": 0, 
+    "EXPGAIN": 0,
+    "PARADIANS": 0
+}
+
+# Class Dictionaries
+warrior_dict = {
+    "POWER": 30,
+    "DEFENSE": 30,
+    "CLASS_": "Warrior"
+}
+
+ranger_dict = {
+    "CRIT_CHANCE":15,
+    "CLASS_": "Ranger"
+}
+
+mage_dict = {
+    "POWER": 40,
+    "DEFENSE": -5,
+    "CLASS_": "Mage"
+}
+
+# Ability Dictionaries
+
+abilities = {
+    "Warrior": {
+        "ABILITY_1": {
+            "NAME": "Thrash",
+            "TOOLTIP": "A thrashing attack that Triples attack power, but reduces defence by 20%``` https://i.pinimg.com/originals/5b/31/0a/5b310a23d4d2c7879d215eb477590ea6.gif ```diff",
+            "PLAYER":{
+                "POWER": 300,
+                "DEFENSE": -20
+            }
+        },
+
+        "ABILITY_2": {
+            "NAME": "Warrior Cry",
+            "TOOLTIP": "A piercing cry which reduces the defense of enemies by 5%, and increases the power of the user by 30, defense by 5%, and health by 10%",
+            "PLAYER": {
+                "POWER": 30,
+                "DEFENSE": 5,
+                "HEALTH": 10
+            },
+            "ENEMY":{
+                "DEFENSE": -5
+            }
+        }
+    },
+
+    "Ranger": {
+        "ABILITY_1": {
+            "NAME": "Ballista Shot",
+            "TOOLTIP": "Fires a powerful piercing shot which does 250% of power``` https://static.wikia.nocookie.net/terraria_gamepedia/images/e/ee/Ballista_Cane_%28demo%29.gif ```diff",
+            "PLAYER": {
+                "POWER": 250
+            }
+        },
+
+        "ABILITY_2": {
+            "NAME": "Sharp Eye",
+            "TOOLTIP": "Focusing strength into eyes, critical strike chance is increased by 30%",
+            "PLAYER": {
+                "CRIT_CHANCE": 30
+            }
+        }
+    },
+
+    "Mage":{
+        "ABILITY_1": {
+            "NAME": "Great Sage: Explosion!",
+            "TOOLTIP": "BAKURETSU!!! A powerful explosion that does 300% damage``` https://i.pinimg.com/originals/c4/07/40/c4074087283441de471b78e0fb56cf25.gif ```diff",
+            "PLAYER":{
+                "POWER": 300
+            }
+        },
+
+        "ABILITY_2": {
+            "NAME": "Activate Prepared Magic",
+            "TOOLTIP": "A pre-prepared spell the reduces all enemy stats by 10%, and increases all user stats by 5%",
+            "ENEMY":{
+                "DEFENSE": -10,
+                "POWER": -10,
+                "HEALTH": -10
+            },
+            "PLAYER":{
+                "HEALTH": 5,
+                "DEFENSE": 5,
+                "CRIT_CHANCE": 5
+            }
+        }
+    }
+}
+
+blessings = {
+    "Warrior": {
+        "NAME": "Warrior's Blessing",
+        "TOOLTIP": "Doubles Current Health, increases attack power by 50%, increases defense by 30%",
+        "EFFECTS":{
+            "POWER": 50,
+            "DEFENSE": 30,
+            "HEALTH": 100
+        }
+    },
+    "Ranger": {
+        "NAME": "Ranger's Blessing",
+        "TOOLTIP": "Increases critical hit chance by 20%, increases damage done by critical hit by 30%",
+        "EFFECTS": {
+            "CRITICAL_CHANCE": 20,
+            "CRITICAL_DAMAGE": 30
+        }
+    },
+    "Mage":{
+        "NAME": "Mage's Blessing",
+        "TOOLTIP": "Increases power by 50%",
+        "EFFECTS":{
+            "POWER": 50
+        }
+    }
+}
--- battle_functionality.py
+++ battle_functionality.py
@@ -1,121 +1,5 @@
 from random import choice, randint, sample, shuffle
-# Class Dictionaries
-warrior_dict = {
-    "POWER": 30,
-    "DEFENSE": 30,
-    "CLASS_": "Warrior"
-}
-
-ranger_dict = {
-    "CRIT_CHANCE":15,
-    "CLASS_": "Ranger"
-}
-
-mage_dict = {
-    "POWER": 25,
-    "DEFENSE": -5,
-    "CLASS_": "Mage"
-}
-
-# Ability Dictionaries
-
-abilities = {
-    "Warrior": {
-        "ABILITY_1": {
-            "NAME": "Thrash",
-            "TOOLTIP": "A thrashing attack that Triples attack power, but reduces defence by 20%``` https://i.pinimg.com/originals/5b/31/0a/5b310a23d4d2c7879d215eb477590ea6.gif ```diff",
-            "PLAYER":{
-                "POWER": 300,
-                "DEFENSE": -20
-            }
-        },
-
-        "ABILITY_2": {
-            "NAME": "Warrior Cry",
-            "TOOLTIP": "A piercing cry which reduces the defense of enemies by 5%, and increases the power of the user by 30, defense by 5%, and health by 10%",
-            "PLAYER": {
-                "POWER": 30,
-                "DEFENSE": 5,
-                "HEALTH": 10
-            },
-            "ENEMY":{
-                "DEFENSE": -5
-            }
-        }
-    },
-
-    "Ranger": {
-        "ABILITY_1": {
-            "NAME": "Ballista Shot",
-            "TOOLTIP": "Fires a powerful piercing shot which does 250% of power``` https://static.wikia.nocookie.net/terraria_gamepedia/images/e/ee/Ballista_Cane_%28demo%29.gif ```diff",
-            "PLAYER": {
-                "POWER": 250
-            }
-        },
-
-        "ABILITY_2": {
-            "NAME": "Sharp Eye",
-            "TOOLTIP": "Focusing strength into eyes, critical strike chance is increased by 30%",
-            "PLAYER": {
-                "CRIT_CHANCE": 30
-            }
-        }
-    },
-
-    "Mage":{
-        "ABILITY_1": {
-            "NAME": "Great Sage: Explosion!",
-            "TOOLTIP": "BAKURETSU!!! A powerful explosion that does 300% damage``` https://i.pinimg.com/originals/c4/07/40/c4074087283441de471b78e0fb56cf25.gif ```diff",
-            "PLAYER":{
-                "POWER": 300
-            }
-        },
-
-        "ABILITY_2": {
-            "NAME": "Activate Prepared Magic",
-            "TOOLTIP": "A pre-prepared spell the reduces all enemy stats by 10%, and increases all user stats by 5%",
-            "ENEMY":{
-                "DEFENSE": -10,
-                "POWER": -10,
-                "HEALTH": -10
-            },
-            "PLAYER":{
-                "HEALTH": 5,
-                "DEFENSE": 5,
-                "CRIT_CHANCE": 5
-            }
-        }
-    }
-}
-# Classes
-class Player:
-    """Class for the player"""
-    def __init__(self, name="", level=1, tier=1, class_="", health=100, power=10, defense=10, crit_chance=5, ability_1="None", ability_2="None", passive="None", paradians=200, weapon="None", armor="None", player_id=0) -> None:
-        self.name = name
-        self.level = level 
-        self.tier = tier 
-        self.class_ = class_ 
-        self.health = health
-        self.power = power 
-        self.defense = defense 
-        self.crit_chance = crit_chance 
-        self.ability_1 = ability_1
-        self.ability_2 = ability_2
-        self.passive = passive
-        self.paradians = paradians
-        self.weapon = weapon 
-        self.armor = armor 
-        self.player_id = player_id
-
-class Enemy:
-    """Class for the Enemy"""
-    def __init__(self, name="Enemy", health=100, power=15, defense=10, expgain=100, paradians=100) -> None:
-        self.name = name
-        self.health = health 
-        self.power = power
-        self.defense = defense
-        self.expgain = expgain
-        self.paradians = paradians
+from battle_dictionaries import *
 
 # battle_emojis = {
 #         "⚔️": "Attack",
@@ -164,7 +48,7 @@ class BattleHandler:
         
         power -= (defender.defense // 2)
         if power < 0: power = 1
-        defender.health -= power
+        defender["HEALTH"] -= power
 
         if is_crit:
             return f"IT'S A CRIT. {attacker.name} attacked {defender.name} and did a whoppin {power} damage"
@@ -179,29 +63,38 @@ class BattleHandler:
         if not will_heal:
             return f"{attacker.name} tried to heal but was in a hurry and spilt the potion"
 
-        heal_amount = randint((attacker.health//10), (attacker.health//5))
-        attacker.health += heal_amount
+        heal_amount = randint((attacker["HEALTH"]//10), (attacker["HEALTH"]//5))
+        attacker["HEALTH"] += heal_amount
         return f"{attacker.name} healed for {heal_amount} health"
 
 
     def handle_ability_1(self, attacker, defender):
-        ability = abilities[attacker.class_]["ABILITY_1"]
+        ability = abilities[attacker["CLASS"]]["ABILITY_1"]
         msg = self.handle_ability(attacker, defender, ability)
         return msg
 
     def handle_ability_2(self, attacker, defender):
-        ability = abilities[attacker.class_]["ABILITY_2"]
+        ability = abilities[attacker["CLASS"]]["ABILITY_2"]
         msg = self.handle_ability(attacker, defender, ability)
         return msg        
 
-    def handle_blessing(self):
-        pass 
+    def handle_blessing(self, attacker):
+        blessing = blessings[attacker["CLASS"]]
+        for k, v in blessing.items():
+            value = attacker.get(k)
+            if not value:
+                attacker[k] = v
+            else:
+                attacker[k] = (v/100 * value) + value
+
+        msg = f"I, {attacker['name']}, have been blessed. {blessing['NAME']}: {blessing['TOOLTIP']}"
+        return msg
 
     def handle_running(self):
         pass
 
     def get_power(self, attacker):
-        return randint(attacker.power-5, attacker.power+6)
+        return randint(attacker["POWER"]-5, attacker["POWER"]+6)
 
     def handle_ability(self, attacker, defender, ability):
         msg = f"Ability Activate: {ability['NAME']}: {ability['TOOLTIP']}"
@@ -218,7 +111,7 @@ class BattleHandler:
                 setattr(defender, k.lower(), getattr(defender, k.lower()) + v)
 
         if ability["PLAYER"].get("POWER", None):
-            defender.health -= power 
+            defender["HEALTH"] -= power 
             msg += f"\n+ {defender.name} took {power} damage from {attacker.name}'s {ability['NAME']}"
 
         return msg
