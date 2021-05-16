43461fab1834074fa93b8d9c74caf8d9142dbaca
Isaiah-Sama <zeldaplayergl@gmail.com>
2021-05-16 14:50:54 -0400

    Added more tips Fixed a bug with damage training

--- tips.py
+++ tips.py
@@ -1,5 +1,9 @@
 tips = [
     "Lost some 💙? Legend has it that being active increases your health gradually.",
     "Want to take part in a Battle style RPG? Simply type <>createprofile to get started",
-    "Did you know that you can use <>menu to view a menu interface for battle commands?"
+    "Did you know that you can use <>menu to view a menu interface for battle commands?",
+    "Want to join the server where it all began? Use <>parade to get a link",
+    "Did you know that you can get pets? Use <>getpet to get one",
+    "Legend has it that Battle Commands are still in Beta Testing.",
+    "Did you know that you can make suggestions using <>suggest?"
 ]
